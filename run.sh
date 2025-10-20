1547ae2f9a9cc15fef14f1c200788a323489f6af
beeinger <sulecki.maciek@gmail.com>
2025-10-20 12:52:07 +0100

    pie and proving

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ bun run input/src/index.ts &&\
 
 hdp-cli fetch-proofs
 
-hdp-cli sound-run -m target/dev/hdp_bytecode_executable.compiled_contract_class.json --inputs ./input.json --print_output
+hdp-cli sound-run -m target/dev/hdp_bytecode_executable.compiled_contract_class.json --inputs ./input.json --print_output --cairo_pie ./pie.zip
+
+# If you're feeling brave:
+# hdp-cli sound-run -m target/dev/hdp_bytecode_executable.compiled_contract_class.json --inputs ./input.json --proof_mode --stwo_proof ./proof.json
