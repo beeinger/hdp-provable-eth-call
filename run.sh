f82d65ce194423dd9ca642f299a73670d8259fb0
beeinger <sulecki.maciek@gmail.com>
2025-11-11 15:43:42 -0300

    small fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ scarb build --verbosity no-warnings &&\
 hdp dry-run -m ./target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output &&\
 hdp fetch-proofs &&\
 hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode &&\
-hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode
+hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output
 
 # hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --cairo_pie ./pie.zip
 
