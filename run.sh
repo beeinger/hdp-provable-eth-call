931bd5ec1c6c7c190fa00bfc7ccbef5e81dac4e7
beeinger <sulecki.maciek@gmail.com>
2025-11-11 14:29:48 -0300

    cleanup

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 scarb build --verbosity no-warnings &&\
-hdp-cli dry-run -m ./target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output &&\
-hdp-cli fetch-proofs &&\
-hdp-cli sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode
-# hdp-cli sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --cairo_pie ./pie.zip
+hdp dry-run -m ./target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output &&\
+hdp fetch-proofs &&\
+hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode &&\
+hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode
+
+# hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --cairo_pie ./pie.zip
 
 
 #? If you're feeling brave (probably need like 500GB of RAM, for sure more than 70GB):
-# hdp-cli sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode --stwo_proof ./proof.json
+# hdp sound-run -m target/dev/hdp_provable_eth_call_executable.compiled_contract_class.json --print_output --proof_mode --stwo_proof ./proof.json
